925d631653f8992a38791423441e026e073c3f23
Elliot Shank <perl@galumph.com>
2011-01-31 12:09:54 +0000

    Copyright year update in PPIx-Utilities and Perl-Critic.
    git-svn-id: http://perlcritic.tigris.org/svn/perlcritic/trunk/distributions/PPIx-Utilities@4022 7ac2fd03-a204-0410-b7b1-b9f6acf132f9

--- lib/PPIx/Utilities/Statement.pm
+++ lib/PPIx/Utilities/Statement.pm
@@ -197,7 +197,7 @@ Thomas R. Wyant, III C<< <wyant at cpan dot org> >>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2009-2010 Thomas R. Wyant, III.  All rights reserved.
+Copyright (c) 2009-2011 Thomas R. Wyant, III.  All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.  The full text of this license
